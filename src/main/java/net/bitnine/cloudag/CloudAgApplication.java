25353df4151c85b1da7ddc67a0203f67400eb8f0
Changhyun Kim <chkim@bitnine.net>
2021-05-26 11:24:46 +0900

    create cloud-ag project

---
+++ src/main/java/net/bitnine/cloudag/CloudAgApplication.java
@@ -0,0 +1,13 @@
+package net.bitnine.cloudag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAgApplication.class, args);
+    }
+
+}
